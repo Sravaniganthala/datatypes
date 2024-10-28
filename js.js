b16d6accabd99d77af68492d7c41ed53b06ff00e
Sravaniganthala <146628579+Sravaniganthala@users.noreply.github.com>
2024-10-28 09:34:32 +0530

    Add files via upload

---
+++ js.js
@@ -0,0 +1,30 @@
+var dad;
+var mom;
+var me;
+var sis;
+var family=["dad","mom","me","sis"];
+var details={
+    dad:{
+        name:"venkateswararao",
+        age:"43",
+        occupation:"farming",
+    },
+    mom:{
+       name:"krishna",
+       age:"33",
+       occupation:"house wife",
+    },
+    me:{
+        name:"sravs",
+        age:"22",
+    },
+    sis:{
+        name:"sandy",
+        age:"19"
+    }
+}
+console.log(dad);
+console.log(mom);
+console.log(me);
+console.log(sis);
+console.log(family.length);
